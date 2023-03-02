18e9bbf9799674f45956d84f10fb80d33a0a6e46
duc.nc <duc.nc97@gmail.com>
2023-03-02 15:16:01 +0700

    a

--- tests/Feature/User/ForgotPasswordTest.php
+++ tests/Feature/User/ForgotPasswordTest.php
@@ -3,9 +3,7 @@
 namespace Tests\Feature\User;
 
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Notification;
 use Tests\TestCase;
 
