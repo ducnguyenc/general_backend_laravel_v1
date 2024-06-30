4140370005cb686d393e36101097a4226d6d67c6
ducnguyenc <70254467+ducnguyenc@users.noreply.github.com>
2024-06-30 09:51:22 +0700

    view ddd

---
+++ resources/views/a.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <pre class="main">
+    {{ $b }}
+    </pre>
+</body>
+
+</html>
+
+<style>
+    body {
+        background-color: gray;
+    }
+
+    .main {
+        background-color: rgb(23 25 35);
+        color: rgb(181 181 189);
+        margin: auto;
+        word-wrap: break-word;
+        white-space: pre-wrap;
+        font-size: 1rem;
+    }
+</style>
